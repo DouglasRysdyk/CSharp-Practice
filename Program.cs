6e54ecaefdbff75ec9a867c9b1a01e7fc3932373
Douglas Rysdyk <douglasrysdyk@gmail.com>
2022-08-19 14:15:24 -0400

    Added tests.  Practicing using VSCode's Git integration.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using System.Runtime.Serialization;
+using System.Reflection.Emit;
+using System.Data;
+//using Internal;
+using System.Linq.Expressions;
+using System;
 using Classes;
 
 namespace dougr
@@ -9,6 +14,18 @@ namespace dougr
         {
             var account = new BankAccount("Doug", 1000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            BankAccount invalidAccount;  
+            try 
+            {
+                invalidAccount = new BankAccount("INVALID", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance.");
+                Console.WriteLine(e.ToString());
+                return;
+            }
 
             Console.WriteLine(account.Balance);
             account.MakeDeposite(100, DateTime.Now, "Friend paid me back");
